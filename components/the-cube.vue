afe67032a5a652adbd9be996b2a0d2f34e4c1cec
Tevin Rivera <tevin@bradsdeals.com>
2024-09-19 14:35:36 -0600

    Init Nuxt Project of NinetyPho.com
    - init nuxt project
    - create basic routing for home and contact pages
    - create cube component with base styling
    - create main.css

---
+++ components/the-cube.vue
@@ -0,0 +1,119 @@
+<template>
+  <NuxtLink :to="to" class="cube" :target="target">
+    <div class="face front">{{ title }}</div>
+    <div class="face back"></div>
+    <div class="face left"></div>
+    <div class="face right"></div>
+    <div class="face top"></div>
+    <div class="face bottom"></div>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  to: string
+  title: string
+  target?: '_blank' | '_parent' | '_self' | '_top'
+}
+
+defineProps<Props>()
+</script>
+<style scoped>
+.cube {
+  display: block;
+  width: 100px;
+  height: 100px;
+  position: relative;
+  transform-style: preserve-3d;
+  transition: transform 250ms ease-in;
+  cursor: pointer;
+}
+
+.cube:hover {
+  width: 100px;
+  height: 100px;
+}
+
+.cube--music {
+  transform: rotateX(232deg) rotateY(178deg) rotateZ(218deg);
+  z-index: 10;
+}
+
+.cube--music .front {
+  background-color: #636363;
+  color: white;
+}
+
+.cube--merch {
+  transform: rotateX(216deg) rotateY(131deg) rotateZ(182deg);
+  margin-top: 80px;
+  z-index: 20;
+}
+
+.cube--merch .front {
+  background-color: #898989;
+  color: white;
+}
+
+.cube--visuals {
+  transform: rotateX(150deg) rotateY(140deg) rotateZ(180deg);
+  margin-top: 160px;
+  z-index: 30;
+}
+
+.cube--visuals .front {
+  background-color: #d2d2d2;
+  color: black;
+  font-size: 18px;
+}
+
+.cube--contact {
+  transform: rotateX(152deg) rotateY(151deg) rotateZ(189deg);
+  margin-top: 264px;
+  z-index: 40;
+}
+
+.cube--contact .front {
+  background-color: #f4f4f4;
+  color: black;
+  font-size: 18px;
+}
+
+.face {
+  position: absolute;
+  width: 100px;
+  height: 100px;
+  background-color: #000;
+  color: #fff;
+  font-size: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 5px solid black;
+  text-transform: uppercase;
+}
+
+.front {
+  transform: translateZ(50px);
+}
+
+.back {
+  transform: rotateY(180deg) translateZ(50px);
+}
+
+.left {
+  transform: rotateY(-90deg) translateZ(50px);
+}
+
+.right {
+  transform: rotateY(90deg) translateZ(50px);
+}
+
+.top {
+  transform: rotateX(90deg) translateZ(50px);
+}
+
+.bottom {
+  transform: rotateX(-90deg) translateZ(50px);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="wrapper flex-center">
+    <div class="container">
+      <the-cube
+        v-for="item in headerItems"
+        :key="item.title"
+        :title="item.title"
+        :class="`cube--${item.title.toLowerCase()}`"
+        :to="item.to"
+        :target="item.target"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const headerItems: Ref<
+  {
+    title: string
+    to: string
+    target: '_blank' | '_parent' | '_self' | '_top'
+  }[]
+> = ref([
+  {
+    title: 'Music',
+    to: 'https://google.com',
+    target: '_blank',
+  },
+  {
+    title: 'Merch',
+    to: 'https://shop.ninetypho.com',
+    target: '_self',
+  },
+  {
+    title: 'Visuals',
+    to: 'https://youtube.com',
+    target: '_blank',
+  },
+  {
+    title: 'Contact',
+    to: '/contact',
+    target: '_self',
+  },
+])
+</script>
+
+<style scoped>
+.wrapper {
+  background: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  line-height: 1.5;
+  width: 100%;
+  height: 100%;
+  min-width: 100vw;
+}
+
+.container {
+  perspective: 1000px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+}
+
+@media (min-width: 1024px) {
+  .wrapper {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+}
+</style>
