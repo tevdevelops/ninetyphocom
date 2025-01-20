2fbd4198b76fd85ab69cade42f01058a5b941d0f
Tevin Rivera <tevin@bradsdeals.com>
2025-01-20 12:43:31 -0700

    - add gtag for analytics - update vulnerable dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  modules: ['@nuxt/image']
+  modules: ['@nuxt/image', 'nuxt-gtag'],
+  gtag: {
+    id: 'G-MGLRSKK33B'
+  }
 })
